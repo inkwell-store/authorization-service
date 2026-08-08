922e918411f1ba366759112ddc262fbaedbb4486
Wilson-Barbosa <wilsond_barbosa@outlook.com>
2026-08-07 23:49:30 -0300

    create project

---
+++ src/test/java/com/wilson/inkwell/authorization/AuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilson.inkwell.authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
